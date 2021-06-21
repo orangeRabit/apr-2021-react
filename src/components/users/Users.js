452dee70f66c03be5861f9b08941a5f5796a57a8
GrayHead <ss.zhuravlov@gmail.com>
2021-06-21 21:38:20 +0300

    mon l1 part 1

---
+++ src/components/users/Users.js
@@ -0,0 +1,27 @@
+import {useEffect, useState} from 'react';
+
+export default function Users() {
+
+
+	let [usersList, setUsersList] = useState([]); // [.....user]
+
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(value => value.json())
+			.then(response => {
+				console.log(response);
+				setUsersList(response);
+			});
+	}, []);
+
+	return (
+		<div>
+			{
+				usersList
+					.map(value => <div>{value.id} - {value.name}</div>)
+
+			}
+
+		</div>
+	);
+}
