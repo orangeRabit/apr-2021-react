0cc2d2c0485ab7e15471573c7fe4c61b3c2da91e
orangeRabit <pulyak.a.v@gmail.com>
2021-06-22 18:24:34 +0300

    add button hide/show

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,11 +1,11 @@
 import './User.css'
+import {useState} from "react";
 
 export default function User({item: {id, name, username, email, address, phone, company}}) {
-
+    let [toggle, setToggle] = useState('hide');
     let userCard = 'userCard'
-    let compCard  ='compCard'
-    let userAdres  ='userAdres'
-
+    let compCard = 'compCard'
+    let userAdres = 'userAdres'
 
 
     return (
@@ -15,22 +15,34 @@ export default function User({item: {id, name, username, email, address, phone, 
             <h3>Нікнейм - {username}</h3>
             <p>Електронна адреса - {email}</p>
             <p>Телефон - {phone}</p>
+            <div className={toggle}>
 
-            <div className={userAdres}>
-                <h3>Місце проживання</h3>
-                <p>Місто - {address.city}</p>
-                <p>Вулиця - {address.street}</p>
-                <p>Поштова скринька - {address.zipcode}</p>
+                <div className={userAdres}>
+                    <h3>Місце проживання</h3>
+                    <p>Місто - {address.city}</p>
+                    <p>Вулиця - {address.street}</p>
+                    <p>Поштова скринька - {address.zipcode}</p>
+                </div>
+
+                <div className={compCard}>
+                    <h3>Місце роботи</h3>
+                    <p>Назва компанії - {company.name}</p>
+                    <p>Слоган - {company.catchPhrase}</p>
+                    <p>bs - {company.bs}</p>
+
+                </div>
+
             </div>
 
-            <div className={compCard}>
-                <h3>Місце роботи</h3>
-                <p>Назва компанії - {company.name}</p>
-                <p>Слоган - {company.catchPhrase}</p>
-                <p>bs - {company.bs}</p>
-
-            </div>
-
+            <button className='info'
+                    onClick={() => {
+                        if (toggle === 'show') {
+                            setToggle('hide');
+                        } else if (toggle === 'hide') {
+                            setToggle('show');
+                        }
+                    }}>Додаткова інформація
+            </button>
 
         </div>
     )
